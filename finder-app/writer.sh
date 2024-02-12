a8142540a71011c4c91af4a18297b6b5452de7c0
HappyMadtec <happymadtec@hotmail.com>
2024-02-12 20:13:18 +0100

    debugging

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+echo "Current directory: $(pwd)"
+if [ -x ./writer ]; then
+    echo "./writer exists and is executable"
+else
+    echo "./writer does not exist or is not executable"
+fi
 ./writer $1 $2
 exit $?
