14bdef633352559a914ca1ff241e87ac69c22b81
HappyMadtec <happymadtec@hotmail.com>
2024-02-14 15:36:14 +0100

    changes

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-if [ -z "$1" ] || [ -z "$2" ]; then
+if [ "$#" -ne 2 ]; then
     echo "There has to be two arguments"
     exit 1
 fi
 
 filedir="$1"
 
+if [ ! -d "$filedir" ]; then
+    echo "Directory does not exist"
+    exit 1
+fi
+
 searchstr=$(find "$filedir" -type f | wc -l)
+value_line=0
+
 for file in "$filedir"/*; do
     occurrences=$(grep -o "$2" "$file" | wc -l)
     ((value_line += occurrences))
 done
 
-echo "The number of files are $searchstr and the number of matching lines are $value_line"
+echo "The number of files in $filedir is $searchstr and the number of matching lines are $value_line"
 
